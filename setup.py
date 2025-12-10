ed70781a5d888c8f88eaa4a6e88d1efe6e66c692
Ryan Shellberg <shellberg@zestworld.com>
2025-12-09 19:15:25 -0600

    Update redis dependency version constraints

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ with open(rel("dramatiq_dashboard", "__init__.py"), "r") as f:
 dependencies = [
     "dramatiq[redis]>=1.6",
     "jinja2",
-    "redis>=5.0,<7.0",
+    "redis>=5.0",
 ]
 
 extra_dependencies = {}
